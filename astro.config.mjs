31ecd3f3eb1757362880c72a2cd17fc3812eef5b
Pim Haring <hello@studiomoondust.com>
2023-01-29 16:53:58 +0100

    first

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import alpinejs from '@astrojs/alpinejs';
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    alpinejs(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    partytown(),
+    sitemap(),
+  ],
+});
