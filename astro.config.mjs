1e68d5728cde82579bb2b5ed8ec9dccc11c2bcb8
Pim Haring <hello@studiomoondust.com>
2023-03-26 12:38:24 +0200

    favicon and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://moondust.com',
   integrations: [
     alpinejs(),
     tailwind({
